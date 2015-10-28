08bef8558c7e88f88aae5d8b4fa442ba4f7b9da8
Marco van Gaal <marco@proxx.nl>
2015-10-28 22:56:33 +0100

    added return object switch (returns object instead of dataTable)

--- Network/Invoke-SQL.ps1
+++ Network/Invoke-SQL.ps1
@@ -3,23 +3,31 @@
 	param(
 		[string] $ComputerName = ".\SQLEXPRESS",
 		[string] $Database = "proxxdb",
-		[string] $User = "dataread",
-		[string] $Passw = "dataread",
-		[string] $Query = $(throw "Please specify a query.")
+        [Alias("User")]
+		[string] $Username = $(throw "Please specify a User."),
+        [Alias("Passw")]
+		[string] $Password = $(throw "Please specify a Password."),
+		[string] $Query = $(throw "Please specify a query."),
+        [Switch] $ReturnObject
 	)
-
-	Add-Type -AssemblyName System.Data
-	$SqlConnection = New-Object System.Data.SqlClient.SqlConnection
-	#$SqlConnection.ConnectionString = "Server = $SQLServer; Database = $SQLDBName; Integrated Security = True"
-	$SqlConnection.ConnectionString = "Server=$ComputerName; Database=$Database; User ID=$User; Password=$Passw;"
-	$SqlCmd = New-Object System.Data.SqlClient.SqlCommand
-	$SqlCmd.CommandText = $Query
-	$SqlCmd.Connection = $SqlConnection
-	$SqlAdapter = New-Object System.Data.SqlClient.SqlDataAdapter
-	$SqlAdapter.SelectCommand = $SqlCmd
-	$DataSet = New-Object System.Data.DataSet
-	$Rows = $SqlAdapter.Fill($DataSet)
-	$SqlConnection.Close()
-
-	return @(,($DataSet.Tables[0]))
+    Begin {
+	    Add-Type -AssemblyName System.Data
+	    $SqlConnection = New-Object System.Data.SqlClient.SqlConnection
+	    #$SqlConnection.ConnectionString = "Server = $SQLServer; Database = $SQLDBName; Integrated Security = True"
+	    $SqlConnection.ConnectionString = "Server=$ComputerName; Database=$Database; User ID=$User; Password=$Passw;"
+	    $SqlCmd = New-Object System.Data.SqlClient.SqlCommand
+	    $SqlCmd.CommandText = $Query
+	    $SqlCmd.Connection = $SqlConnection
+	    $SqlAdapter = New-Object System.Data.SqlClient.SqlDataAdapter
+	    $SqlAdapter.SelectCommand = $SqlCmd
+	    $DataTable = New-Object System.Data.DataTable
+    }
+    Process {
+    
+        $Rows = $SqlAdapter.Fill($DataTable)
+    }
+    End {
+	    $SqlConnection.Close()
+	    return @(,($DataTable))
+    }
 }
