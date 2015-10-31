2e91a6d5343adec0bafd30aa226abaddc61948c7
Proxx <marco@proxx.nl>
2015-10-31 19:27:54 +0100

    Added lockscreen function

---
+++ Common/Lock-Screen.ps1
@@ -0,0 +1,33 @@
+﻿Function Lock-Screen {
+    [CmdletBinding()]
+    Param(
+        [String] $Message="Sample Message",
+        [Int] $Delay=10,
+        [Parameter(ParameterSetName="PassThru")]
+        [Switch] $PassThru
+    )
+    $window = New-Object Windows.Window
+    $label = New-Object Windows.Controls.Label
+
+    $label.Content = $Message
+    $label.FontSize = 60
+    $label.FontFamily = 'Consolas'
+    $label.Background = 'Transparent'
+    $label.Foreground = 'Red'
+    $label.HorizontalAlignment = 'Center'
+    $label.VerticalAlignment = 'Center'
+
+    $Window.AllowsTransparency = $True
+    $Window.Opacity = .7
+    $window.WindowStyle = 'None'
+    $window.Content = $label
+    $window.Left = $window.Top = 0
+    $window.WindowState = 'Maximized'
+    $window.Topmost = $true
+
+    $null = $window.Show()
+    if ($PassThru) { Return $window }
+    Start-Sleep -Seconds $Delay
+    $window.Close()
+
+}
